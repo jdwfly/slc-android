c26c44af39de9f013c5cb5d61ec2da52a288b7f9
Joe Wheaton <joseph.wheaton@gmail.com>
2014-01-29 11:17:07 -0800

    Removed unneeded code, reworked the livestream code, disabled the speakers tab for now.

--- Resources/ui/common/mainTabView.js
+++ Resources/ui/common/mainTabView.js
@@ -27,44 +27,35 @@ exports.mainTabView = function() {
     title: 'Maps',
     window: new mapsWindow()
   });
+  instance.addTab(mapsTab);
   
   var newsWindow = require('ui/common/newsWindow').newsWindow;
   var newsTab = Ti.UI.createTab({
     title: 'Videos',
     window: new newsWindow()
   });
+  instance.addTab(newsTab);
   
+  /*
+  Until the conference is closer this tab will be disabled.
   var speakersWindow = require('ui/common/speakersWindow').speakersWindow;
   var speakersTab = Ti.UI.createTab({
     title: 'Speakers',
     window: new speakersWindow()
   });
-  
-  var liveWindow = require('ui/common/liveWindow').liveWindow;
-  var liveTab = Ti.UI.createTab({
-    title: 'Live',
-    window: new liveWindow()
-  });
-  
-  instance.addTab(mapsTab);
-  instance.addTab(newsTab);
   instance.addTab(speakersTab);
-  if (globals.osname === 'iphone' || globals.osname === 'ipad') {
-    instance.addTab(liveTab);
-  } else if (globals.osname === 'android' && Ti.Platform.Android.API_LEVEL >= 14) {
+  */
+  
+  if (globals.osname === 'android' && Ti.Platform.Android.API_LEVEL >= 14) {
+    var liveWindow = require('ui/common/liveWindow').liveWindow;
+    var liveTab = Ti.UI.createTab({
+      title: 'Live',
+      window: new liveWindow()
+    });
     instance.addTab(liveTab);
   }
-  
-  // Workaround for not knowing the current tab
-  // http://developer.appcelerator.com/question/98501/titaniumuicurrenttab-is-null
+
   instance.addEventListener('focus', function(e) {
-    instance._activeTab = e.tab;
-    instance._activeTabIndex = e.index;
-    if (instance._activeTabIndex == -1) return;
-    //Ti.API.info(instance._activeTabIndex);
-    //Ti.API.info(instance._activeTab.title);
-    Ti.API._activeTab = instance._activeTab;
-    //Ti.API.info(Ti.API._activeTab.title);
     activity.invalidateOptionsMenu();
   });
   
