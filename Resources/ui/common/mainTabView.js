72795bdf72c07e9e7a84ffdb040147db0a9e2acb
Joe Wheaton <joseph.wheaton@gmail.com>
2014-05-30 10:05:48 -0700

    Restructure the tab view to be more easily configured.

--- Resources/ui/common/mainTabView.js
+++ Resources/ui/common/mainTabView.js
@@ -3,54 +3,72 @@ exports.mainTabView = function() {
   var instance = Ti.UI.createTabGroup();
   var apiLevel = Ti.Platform.Android.API_LEVEL, 
       activity;
-  /*
-  // Not Used after conference
-  var scheduleWindow = require('ui/common/scheduleWindow').scheduleWindow;
-  var scheduleTab = Ti.UI.createTab({  
-    icon: 'data/11-clock.png',
-    title: 'Schedule',
-    window: new scheduleWindow()
-  });
-  instance.addTab(scheduleTab);
-  */
-  var sessionWindow = require('ui/common/sessionWindow').window;
-  var sessionTab = Ti.UI.createTab({
-    title: 'Sessions',
-    window: new sessionWindow()
-  });
-  instance.addTab(sessionTab);
-
-  var mapsWindow = require('ui/common/mapsWindow').mapsWindow;
-  var mapsTab = Ti.UI.createTab({
-    title: 'Maps',
-    window: new mapsWindow()
-  });
-  instance.addTab(mapsTab);
   
-  var newsWindow = require('ui/common/newsWindow').newsWindow;
-  var newsTab = Ti.UI.createTab({
-    title: 'Videos',
-    window: new newsWindow()
-  });
-  instance.addTab(newsTab);
+  // Grab the config.json file
+  var configFile = Ti.Filesystem.getFile("config.json");
+  var preParseData = (configFile.read().text);
+  var config = JSON.parse(preParseData);
   
-  /*
-  Until the conference is closer this tab will be disabled.
-  var speakersWindow = require('ui/common/speakersWindow').speakersWindow;
-  var speakersTab = Ti.UI.createTab({
-    title: 'Speakers',
-    window: new speakersWindow()
-  });
-  instance.addTab(speakersTab);
-  */
+  if (config.tabs.schedule === true){
+    var scheduleWindow = require('ui/common/scheduleWindow').scheduleWindow;
+    var scheduleTab = Ti.UI.createTab({
+      icon: 'data/11-clock.png',
+      title: 'Schedule',
+      window: new scheduleWindow()
+    });
+    instance.addTab(scheduleTab);
+  }
+  
+  if (config.tabs.sessions === true) {
+    var sessionWindow = require('ui/common/sessionWindow').window;
+    var sessionTab = Ti.UI.createTab({
+      icon: 'data/120-headphones.png',
+      title: 'Sessions',
+      window: new sessionWindow()
+    });
+    instance.addTab(sessionTab);
+  }
+  
+  if (config.tabs.maps === true) {
+    var mapsWindow = require('ui/common/mapsWindow').mapsWindow;
+    var mapsTab = Ti.UI.createTab({
+      icon: 'data/103-map.png',
+      title: 'Maps',
+      window: new mapsWindow()
+    });
+    instance.addTab(mapsTab);
+  }
+  
+  if (config.tabs.news === true) {
+    var newsWindow = require('ui/common/newsWindow').newsWindow;
+    var newsTab = Ti.UI.createTab({
+      icon: 'data/45-movie-1.png',
+      title: 'Videos',
+      window: new newsWindow()
+    });
+    instance.addTab(newsTab);
+  }
+  
+  if (config.tabs.speakers === true) {
+    var speakersWindow = require('ui/common/speakersWindow').speakersWindow;
+    var speakersTab = Ti.UI.createTab({
+      icon: 'data/112-group.png',
+      title: 'Speakers',
+      window: new speakersWindow()
+    });
+    instance.addTab(speakersTab);
+  }
   
   if (globals.osname === 'android' && Ti.Platform.Android.API_LEVEL >= 14) {
-    var liveWindow = require('ui/common/liveWindow').liveWindow;
-    var liveTab = Ti.UI.createTab({
-      title: 'Live',
-      window: new liveWindow()
-    });
-    instance.addTab(liveTab);
+    if (config.tabs.live === true) {
+      var liveWindow = require('ui/iphon/liveWindow').liveWindow;
+      var liveTab = Ti.UI.createTab({
+        icon: 'data/69-display.png',
+        title: 'Live',
+        window: new liveWindow()
+      });
+      instance.addTab(liveTab);
+    }
   }
 
   instance.addEventListener('focus', function(e) {
