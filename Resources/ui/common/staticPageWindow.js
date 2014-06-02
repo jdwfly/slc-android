48ae3a82a97f4027d10279b5a784e0321bb5b117
Joe Wheaton <joseph.wheaton@gmail.com>
2014-06-02 16:58:56 -0700

    Fix for memory leak due to closing the web view too early.

--- Resources/ui/common/staticPageWindow.js
+++ Resources/ui/common/staticPageWindow.js
@@ -2,21 +2,22 @@ var globals = require('lib/globals');
 exports.staticPageWindow = function(opts) {
   var instance = Ti.UI.createWindow({
     title: opts.title,
-    backgroundColor: '#eeeeee'
+    backgroundColor: '#eeeeee',
+    softKeyboardOnFocus: Titanium.UI.Android.SOFT_KEYBOARD_HIDE_ON_FOCUS
   });
   
   var webView = Ti.UI.createWebView({
-    url: '/ui/static/' + opts.arg,
-    scalesPageToFit: true
+    url: '/ui/static/' + opts.arg
   });
   instance.add(webView);
   
-  if (globals.osname === 'android') {
-    instance.addEventListener('android:back', function(e) {
-      webView = null;
-      this.close();
-    });
-  }
+  instance.addEventListener('android:back', function(e) {
+    this.close();
+  });
+  
+  instance.addEventListener('open', function(e) {
+    Ti.UI.Android.hideSoftKeyboard();
+  });
   
   return instance;
 };
